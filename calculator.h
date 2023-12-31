7a7719266136b7d075e86a6b0e7faea6f5ea1a25
Andrii7259 <103684270+Andrii7259@users.noreply.github.com>
2023-12-31 14:31:39 +0200

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,16 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+    public:
+
+        int Add (double, double);
+        int Sub (double, double);
+        int Mul (double, double);
+        int Mul2(double, double);
+        int Square(double);
+
+};
+
+#endif//CALCULATOR_H
